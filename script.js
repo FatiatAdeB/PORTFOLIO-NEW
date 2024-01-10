ba6a4184b901d87bd09e2d0ddd9129b32ae3d098
FatiatAdeB <adebayofatiat21@gmail.com>
2024-01-10 08:36:33 +0100

    initial commit

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+// mwenu icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector ('navbar');
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 // scroll sections active link
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -23,12 +31,17 @@ window.onscroll = () => {
 let header = document.querySelector('.header');
 
 header.classList.toggle('sticky', window.scrollY > 100);
+
+
+// remove menu icon for navbar
+menuIcon.classList.remove('bx-x');
+navbar.classList.remove('active');
 };
 
 // swiper
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
-    spaceBetween: 30,
+    spaceBetween: 50,
     loop: true,
     grabCursor: true,
     pagination: {
@@ -39,4 +52,25 @@ var swiper = new Swiper(".mySwiper", {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
-  });
+});
+
+// dark mode
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle('bx-sun')
+    document.body.classList.toggle('dark-mode');
+};
+
+
+ScrollReveal({ 
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200 
+});
+
+ScrollReveal().reveal('.home-content, .heading',  { origin: 'top' });
+ScrollReveal().reveal('.home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form',  { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img img',  { origin: 'left' });
+ScrollReveal().reveal('.home-content h3, .home-content p, .about-content',  { origin: 'right' });
